76d83cc7554bf8b18f8e1361c9013f0e44685fba
Insaf Inhaam <insafinhaam732@gmail.com>
2024-07-02 01:53:59 +0530

    chnages until done

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,28 @@
-const OrganizationIdPage = () =>{
-    return(
-        <div>
-            Organiztion Page!
-        </div>
-    )
-}
+import { Info } from "./_components/info";
+import { Separator } from "@/components/ui/separator";
+import { BoardList } from "./_components/board-list";
+import { db } from "@/lib/db";
+import { Board } from "./board";
+import { Form } from "./form";
 
-export default OrganizationIdPage
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany({});
+
+  return (
+    <div className="w-full mb-20">
+      <Form/>
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <Board key={board.id} title={board.title} id={board.id} />
+        ))}
+      </div>
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
+    </div>
+  );
+};
+
+export default OrganizationIdPage;
---
+++ components/form/form-popover.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { FormInput, X } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "../ui/popover";
+
+interface FormPopoverProps {
+  children: React.ReactNode;
+  side?: "left" | "right" | "top" | "bottom";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+}
+
+export const FormPopoverProps = ({
+  children,
+  side = "bottom",
+  align,
+  sideOffset = 0,
+}: FormPopoverProps) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{children}</PopoverTrigger>
+      <PopoverContent
+        align={align}
+        className="w-80 pt-3"
+        side={side}
+        sideOffset={sideOffset}
+      >
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          Create board
+        </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <form className="space-y-4">
+          <div className="space-y-4">
+            {/* <FormInput id="title" label="Board title" type="text" /> */}
+          </div>
+          {/* <FormSubmit className="w-full">Create</FormSubmit> */}
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+};
